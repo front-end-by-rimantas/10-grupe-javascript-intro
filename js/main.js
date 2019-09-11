254b034fa701bd7f4b9d9b17bb53df9808daa739
Rimantas <belauzas@yahoo.com>
2019-09-11 20:40:49 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+console.log(2019);
+
+console.log('kafknvlanfl');
+
+console.log('pirmas', 'antras');
+
+console.log('pirmas', 16155, "kitas", 56116);
+
+console.log(5516, 461615);
+
+console.log(true);
+
+console.log(false);
+
+console.log('--------------');
+
+const vardas = 'Rimantas';
+console.log(vardas);
+
+const amzius = 99;
+console.log(amzius);
+
+const arAmziusTikras = false;
+console.log(arAmziusTikras);
+
+let svecioVardas = 'Onute';
+console.log(svecioVardas);
+
+svecioVardas = 'Ona';
+console.log(svecioVardas);
+
+// type: array
+const laiminguSkaiciuSarasas = [1, 5, 7, 8, 13];
+console.log(laiminguSkaiciuSarasas);
+
+const raides = ['a', 'b', 'c', 'd', 'e'];
+console.log(raides);
+
